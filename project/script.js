5a1d9edaa3f8c383b72665affc503ffbac3c25fd
magilla121 <liev.haroche@sonomaacademy.org>
2018-01-26 13:18:30 -0800

    iohgv

--- project/script.js
+++ project/script.js
@@ -2,8 +2,7 @@ if (annyang){
     document.write("Listening...");
 
 var command = {
-    'background color *thisColor':function(thisColor){
-        document.getElementById("result").innerHTML = ("Background Color: " + thisColor);
+    'background (color) *thisColor':function(thisColor){
         document.getElementById("body").style.backgroundColor = thisColor;
     }
 };
