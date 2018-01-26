6fd511156e5a7e158f3e6b76b5a0582cfde4f06b
magilla121 <liev.haroche@sonomaacademy.org>
2018-01-25 16:42:40 -0800

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,12 +1,13 @@
 
 if(annyang){
     document.write("Listening...");
-var command = {
-    'hello': helloFunction
-};
-
+    
 var helloFunction = function (userName) {
     alert("Hi!");
+};
+    
+var command = {
+    'hello': helloFunction
 };
 
 annyang.addCommands(command);
