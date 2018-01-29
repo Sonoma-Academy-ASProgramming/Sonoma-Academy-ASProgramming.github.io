e907832e79aa4b7d02bc6a38a594070e3f4f615f
Sean-2002 <33432158+Sean-2002@users.noreply.github.com>
2018-01-29 14:57:34 -0800

    Bugs fixed

--- project/GetLunch.js
+++ project/GetLunch.js
@@ -2,8 +2,7 @@ let date;
 
 function getLunch(year, month, day) {
     date = year.toString() + "-" + ((month < 10) ? "0" + month.toString() : month.toString()) + "-" + ((day < 10) ? "0" + day.toString() : day.toString());
-    console.log("Date set to: " + this.date);
-    return "Date set to: " + this.date;
+    console.log("Date set to: " + date);
 
     var url = "http://query.yahooapis.com/v1/public/yql?q=select * from htmlstring where url='http://www.myschooldining.com/sa'&env=store://datatables.org/alltableswithkeys&format=json";
 
@@ -20,26 +19,26 @@ function getLunch(year, month, day) {
 
 
 function GetSoup() {
-    // console.log("Using search string: " + "[day_no=" + this.date + "] div#sonomaacademy_lunch_soup span.item-value");
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_soup span.item-value").text()).trim();
+    // console.log("Using search string: " + "[day_no=" + date + "] div#sonomaacademy_lunch_soup span.item-value");
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_soup span.item-value").text()).trim();
 }
 
 function GetSalad() {
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_entree span.item-value").text()).trim();
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_entree span.item-value").text()).trim();
 }
 
 function GetEntree() {
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_salad span.item-value").text()).trim();
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_salad span.item-value").text()).trim();
 }
 
 function GetSpecialDietEntree() {
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_specialdietentree span.item-value").text()).trim();
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_specialdietentree span.item-value").text()).trim();
 }
 
 function GetSides() {
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_sides span.item-value").text()).trim();
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_sides span.item-value").text()).trim();
 }
 
 function GetDessert() {
-    return ($("[day_no=" + this.date + "] div#sonomaacademy_lunch_dessert span.item-value").text()).trim();
+    return ($("[day_no=" + date + "] div#sonomaacademy_lunch_dessert span.item-value").text()).trim();
 }
